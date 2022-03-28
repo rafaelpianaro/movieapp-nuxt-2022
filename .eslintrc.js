6043506c1192bcfa76c4751932aab2074239898a
Rafael <rafaelpianaro@gmail.com>
2022-03-28 20:35:42 -0300

    multi-word-component-names at .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,6 @@ module.exports = {
   plugins: [],
   // add your custom rules here
   rules: {
-    // 'vue/multi-word-component-names': 0,
+    'vue/multi-word-component-names': 0,
   },
 }
--- components/Hero.vue
+++ components/Hero.vue
@@ -4,7 +4,7 @@
       <div class="text-container">
           <div class="text">
               <span class="mini-heading">Now Streaming</span>
-              <h1><span>Now</span>Streaming</h1>
+              <h1><span>Now</span> Streaming</h1>
               <a href="#movie-grid" class="button">View Movies</a>
           </div>
       </div>
