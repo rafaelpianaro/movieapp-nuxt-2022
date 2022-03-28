672bbc5748139aadc1e8e5e82eb341a1532c9403
Rafael <rafaelpianaro@gmail.com>
2022-03-27 21:39:00 -0300

    getMovies

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,29 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'IndexPage',
+  data() {
+    return {
+      movies: [],
+    }
+  },
+  async fetch() {
+    await this.getMovies()
+  },
+  methods: {
+    async getMovies() {
+      const data = axios.get(
+        `https://api.themoviedb.org/3/movie/now_playing?api_key=881318e9b461104d3c6d1c0828f90c12&language=en-US&page=1`
+      )
+      const result = await data
+      // console.log('result', result.data)
+      result.data.results.forEach((movie) => {
+        this.movies.push(movie)
+      })
+      console.log('array movies',this.movies)
+    }
+  }
 }
 </script>
