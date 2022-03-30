6d12a8dc9e255b7f3e27111bac15fa93bc585a5d
Rafael Pianaro <rafael_pianaro@econeteditora.com.br>
2022-03-30 13:17:50 -0300

    styled _movieid

--- pages/movies/_movieid.vue
+++ pages/movies/_movieid.vue
@@ -21,6 +21,19 @@
                     })
                 }}
             </p>
+            <p class="movie-fact">
+                <span>Duration:</span> {{ movie.runtime }} minutes
+            </p>
+            <p class="movie-fact">
+                <span>Revenue:</span>
+                {{
+                    movie.revenue.toLocaleString('pt-br', {
+                    style: 'currency',
+                    currency: 'BRL',
+                    })
+                }}
+            </p>
+            <p class="movie-fact"><span>Overview:</span> {{ movie.overview }}</p>
         </div>
     </div>
 </div>
@@ -52,5 +65,58 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.single-movie {
+  color: #fff;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 32px 16px;
+  .button {
+    align-self: flex-start;
+    margin-bottom: 32px;
+  }
+  .movie-info {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 32px;
+    color: #fff;
+    @media (min-width: 800px) {
+      flex-direction: row;
+      align-items: flex-start;
+    }
+    .movie-img {
+      img {
+        max-height: 500px;
+        width: 100%;
+        @media (min-width: 800px) {
+          max-height: 700px;
+          width: initial;
+        }
+      }
+    }
+    .movie-content {
+      h1 {
+        font-size: 56px;
+        font-weight: 400;
+      }
+      .movie-fact {
+        margin-top: 12px;
+        font-size: 20px;
+        line-height: 1.5;
+        span {
+          font-weight: 600;
+          text-decoration: underline;
+        }
+      }
+      .tagline {
+        font-style: italic;
+        span {
+          font-style: normal;
+        }
+      }
+    }
+  }
+}
 </style>
